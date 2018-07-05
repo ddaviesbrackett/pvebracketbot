a182137785426854ce96ea0801df18930bc14147
ddaviesbrackett <dan.davies.brackett@gmail.com>
2018-07-05 14:06:14 +0100

    more scaffolding

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ $CONF=['BOT_CHANNEL_ACCESS'=>'',
  'BOT_CHANNEL_SECRET'=>'',
  'LISTEN_ROOM_ID' => '',
  'SHEET_TO_UPDATE' => '',
+ 'DEBUG_ROOM_ID' =>,
 ];
 
 define('SHEETS_CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-pvescribebot.json');
