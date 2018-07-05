6e54af2866a7db6dfd5616180f6ba00139adfe81
ddaviesbrackett <dan.davies.brackett@gmail.com>
2018-07-05 11:02:12 +0100

    scaffolds for secrets files

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$CONF=['BOT_CHANNEL_ACCESS'=>'',
+ 'BOT_CHANNEL_SECRET'=>'',
+ 'LISTEN_ROOM_ID' => '',
+ 'SHEET_TO_UPDATE' => '',
+];
+
+define('SHEETS_CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-pvescribebot.json');
