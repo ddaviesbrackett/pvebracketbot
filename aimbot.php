88b8017c4846e28a4fe6bd87fc694e4305040bc0
ddaviesbrackett <dan.davies.brackett@gmail.com>
2018-10-19 12:08:52 +0100

    I hate php

--- aimbot.php
+++ aimbot.php
@@ -2,6 +2,7 @@
 require_once(__DIR__ . '/config.php');
 function trigger_aimbots($client, $update)
 {
+	global $CONF;
 	if(mb_strtolower($update["count"]) == "flip" || filter_var($update["count"], FILTER_SANITIZE_NUMBER_INT) > $CONF["AIMBOT_THRESHOLD"])
 	{
 		foreach ($CONF["AIMBOT_ROOMS"] as $room)
