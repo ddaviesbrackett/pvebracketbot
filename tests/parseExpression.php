9c59dace5b525d96601b20e5291ad2b455196f4f
ddaviesbrackett <dan.davies.brackett@gmail.com>
2019-03-18 14:22:15 +0000

    don't allow ancillary tools to be executed on the web

--- tests/parseExpression.php
+++ tests/parseExpression.php
@@ -1,5 +1,8 @@
 <?php
 require_once(__DIR__ . '/../parser.php');
+if (php_sapi_name() != 'cli') {
+  throw new Exception('This application must be run on the command line.');
+}
 
 $parser = new PhpPegJs\Parser;
 var_dump($parser->parse($argv[1]));
