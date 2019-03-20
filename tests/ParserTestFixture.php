08d1501e01d26ad5fca9219bf6ed047f0aedb7d6
ddaviesbrackett <dan.davies.brackett@gmail.com>
2019-03-20 15:50:04 +0000

    disallow test execution outside dev

--- tests/ParserTestFixture.php
+++ tests/ParserTestFixture.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 require_once(__DIR__ . '\..\parser.php');
+if (php_sapi_name() != 'cli') {
+  throw new Exception('This application must be run on the command line.');
+}
 
 use PHPUnit\Framework\TestCase;
 
