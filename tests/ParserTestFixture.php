430128f516ea9c2eed618d8ff6c9098180438665
ddaviesbrackett <dan.davies.brackett@gmail.com>
2019-03-20 15:52:27 +0000

    disallow tests on the web

--- tests/ParserTestFixture.php
+++ tests/ParserTestFixture.php
@@ -2,6 +2,10 @@
 declare(strict_types=1);
 require_once(__DIR__ . '\..\parser.php');
 
+if (php_sapi_name() != 'cli') {
+  throw new Exception('This application must be run on the command line.');
+}
+
 use PHPUnit\Framework\TestCase;
 
 class ParserTest extends TestCase
