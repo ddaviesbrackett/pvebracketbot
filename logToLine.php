98abb54612c3841e57996ccc5e11876ae564f802
ddaviesbrackett <dan.davies.brackett@gmail.com>
2019-03-23 15:44:42 +0000

    gobal conf.

--- logToLine.php
+++ logToLine.php
@@ -5,17 +5,18 @@ require_once(__DIR__ . '/vendor/LINEBotTiny.php');
 
 function debugToLine($message)
 {
+    global $CONF;
 	return logToLine($CONF['DEBUG_ROOM_ID'], $message);
 }
 
 function warnToLine($message)
 {
+    global $CONF;
 	return logToLine($CONF['LISTEN_ROOM_ID'], $message);
 }
 
 function logToLine($room,$message)
 {
-
     global $CONF;
     $channelAccessToken = $CONF['BOT_CHANNEL_ACCESS'];
     $channelSecret = $CONF['BOT_CHANNEL_SECRET'];
